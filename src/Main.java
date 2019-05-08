d8e03f922e84a0b94d690b9cdd734c4fdb775ea5
Bassam <BassamA.Mansour@gmail.com>
2019-05-08 21:40:40 +0200

    Modified hibernate cfg

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@ public class Main {
 
 
     public static void main(final String[] args) throws Exception {
+
+        /*User user = new User();
+        user.setFirstName("FName");
+        user.setLastName("LName");
+        user.setAddress("Address");
+        user.setPassword("Password");
+        user.setPhone("123456789");
+        user.setUsername("username");
+        user.setEmail("Email");*/
+
+        //CustomerPanel customerPanel = CustomerPanel.fromNewUser(user);
+
+
+
     }
 
 
