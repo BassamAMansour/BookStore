e1289fc497ea32336072e4ecb6007db16fe03eb5
ma7moud_tarek97 <ma7moud.tarek97@gmail.com>
2019-05-08 23:48:51 +0200

    fix conflict

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,16 @@ public class Main {
 
     public static void main(final String[] args) throws Exception {
 
-        /*User user = new User();
+        User user = new User();
         user.setFirstName("FName");
         user.setLastName("LName");
         user.setAddress("Address");
         user.setPassword("Password");
         user.setPhone("123456789");
         user.setUsername("username");
-        user.setEmail("Email");*/
+        user.setEmail("Email");
 
-        //CustomerPanel customerPanel = CustomerPanel.fromNewUser(user);
+        CustomerPanel customerPanel = CustomerPanel.fromNewUser(user);
     }
 
 }
